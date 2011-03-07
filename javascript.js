730e6a083bdb202c938642e976a51351f183e4ad
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 16:56:07 -0600

    working on making something work

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+var film;
 function hideElement(id){
     document.getElementById(id).style.display = 'none';
 }
@@ -9,6 +10,17 @@ function start(){
     showElement('film-select');
 
 }
-function directions(film){
+function directionsDeveloper(){
+    if (document.getElementById('film-select').value = "k9000");
+        film = 'k9000';
+        showElement('k9000-developer');
+        
+}
+function developer(){
+    var recycle = false;
+    countdown(150); //2:30
+    hideElement('k9000-developer');
     
+}
+function countdown(){
 }
