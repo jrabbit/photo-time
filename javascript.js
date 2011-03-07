6f5241139837eaed0957a78eca21f6878cf1ce5a
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 17:14:28 -0600

    cool stuff

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
 var film;
+var steps = [prewet,developer,waterrinse, stop,waterrinse,fixer,waterrinse,fixer,hypo,waterbath];
+var currentStep;
+/*[function() { return "alpha"; }, function() { return "bravo"; }][0]() */
+
 function hideElement(id){
     document.getElementById(id).style.display = 'none';
 }
@@ -11,15 +15,20 @@ function start(){
 
 }
 function directionsDeveloper(){
+    
     if (document.getElementById('film-select').value = "k9000");
         film = 'k9000';
         showElement('k9000-developer');
         
 }
+function next(){
+    steps[currentStep + 1]()
+    currentStep = ++currentStep
+}
 function developer(){
     var recycle = false;
     countdown(150); //2:30
-    hideElement('k9000-developer');
+    hideElement(film + '-developer');
     
 }
 function countdown(){
