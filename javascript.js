8b47c407d0cec5204258c254e6f45c2f35865e15
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 17:36:46 -0600

    times

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 var film;
 var steps = [prewet,developer,waterrinse, stop,waterrinse,fixer,waterrinse,fixer,hypo,waterbath];
 var reels = 1;
+const temp = 68;
 var currentStep;
 /*[function() { return "alpha"; }, function() { return "bravo"; }][0]() */
 
@@ -16,28 +17,26 @@ function start(){
 
 }
 function directionsDeveloper(){
-    
-    if (document.getElementById('film-select').value = "k9000");
-        film = 'k9000';
-        showElement('k9000-developer');
-        
+    film = document.getElementById('film-select').value
+    showElement('developer')
 }
 function next(){
     steps[currentStep + 1]()
     currentStep = ++currentStep
 }
-function prewet(){
-}
+function prewet(){}
 function waterrinse(){}
 function stop(){}
 function fixer(){}
 function hypo(){}
 function waterbath(){}
 function developer(){
+    var time = {k9000: 9001,toaster: -1, hps400: 8, fp4125:7,delta3200:7,delta100:7,k100:7,k400:8,ktri400:8, a400:8, ilf400:7.5}
     var recycle = false;
-    countdown(150); //2:30
-    hideElement(film + '-developer');
+    countdown(time[film]);
+    hideElement('developer');
     
 }
 function countdown(){
+/* take # minutes and display a countdown */
 }
