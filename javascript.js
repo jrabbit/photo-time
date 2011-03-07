a9aefc8c09eb6b42af781d4d71971d5b853efa3c
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 16:31:06 -0600

    Javascript!

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function hideElement(id){
+    document.getElementById(id).style.display = 'none';
+}
+function showElement(id){
+    document.getElementById(id).style.display = 'block';
+}
+function start(){
+    hideElement('button1');
+    showElement('film-select');
+
+}
+function directions(film){
+    
+}
