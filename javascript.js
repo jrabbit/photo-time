802c745f8c0426724745447aa7f6a0134d037be4
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 18:46:40 -0600

    bugz

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ var steps = [prewet,developer,waterrinse, stop,waterrinse,fixer,waterrinse,hypo,
 var recycle = {developer: false, stop: true, fixer: true, hypo: false}
 var reels = 1;
 const temp = 68;
-var currentStep;
+var currentStep = 0;
 
 /*[function() { return "alpha"; }, function() { return "bravo"; }][0]() */
 
@@ -22,9 +22,14 @@ function start(){
 function directionsDeveloper(){
     film = document.getElementById('film-select').value
     showElement('developer')
+    hideElement('film-select');
+    showElement('n0');
+    showElement('timer');
+    setInterval('developer()', 1000)
 }
 function next(){
     steps[currentStep + 1]()
+    showElement('n' + currentStep)
     currentStep = ++currentStep
 }
 
@@ -35,12 +40,21 @@ function fixer(){}
 function hypo(){}
 function waterbath(){}
 function developer(){
-    var time = {k9000: 9001,toaster: -1, hps400: 8, fp4125:7,delta3200:7,delta100:7,k100:7,k400:8,ktri400:8, a400:8, ilf400:7.5}
+    var time = {k9000: 6,toaster: 6, hps400: 8, fp4125:7,delta3200:7,delta100:7,k100:7,k400:8,ktri400:8, a400:8, ilf400:7.5}
     var recycle = false;
     countdown(time[film]);
-    hideElement('developer');
-    
 }
-function countdown(){
+
+function resolveTime(wait){
+    min = Math.floor( wait / (1000*60))
+    secs  = Math.floor( wait / 1000 );
+    wait_sec = secs - min * 60;
+    return min + ":" +wait_sec
+}
+function countdown(min){
 /* take # minutes and display a countdown */
+    var curtime = Date.now();
+    var end = curtime + (min*1000*60);
+    var wait = end - curtime;
+    document.getElementById('timer').innerHTML = resolveTime(wait) ;
 }
