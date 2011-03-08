2b7dea6e2a6903f1b357c5a46e75b53b075b6446
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-07 21:55:54 -0600

    push bugs

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-// GPLv3 Jrabbit (C) 2011
 var film;
 var steps = [prewet,developer,waterrinse, stop,waterrinse,fixer,waterrinse,hypo,waterbath];
 var recycle = {developer: false, stop: true, fixer: true, hypo: false}
@@ -32,41 +31,47 @@ function directionsDeveloper(){
 function next(){
     steps[currentStep + 1]()
     showElement('n' + currentStep)
+    hideElement('n' + (currentStep -1))
     currentStep = ++currentStep
 }
 
-function prewet(){}
-function waterrinse(){}
+function prewet(){} // No.
+function waterrinse(){} // Are you kidding me?
 function stop(){
-    end = Date.now() + 1000*30 // 30 seconds
-    setInterval(countdown, 1000)
+    end = Date.now() + 1000*30; // 30 seconds
+    setInterval(countdown, 1000);
 }
 function fixer(){
-    end = Date.now() + 5*1000*60 // 5 Minutes
-    setInterval(countdown, 1000)
+    end = Date.now() + 5*1000*60; // 5 Minutes
+    setInterval(countdown, 1000);
 }
 function hypo(){
-    end = Date.now() + 2.5*1000*60 // 2:30
-    setInterval(countdown, 1000)
+    end = Date.now() + 2.5*1000*60; // 2:30
+    setInterval(countdown, 1000);
 }
 function waterbath(){
-    end = Date.now() + 5*1000*60 // 5 Minutes
-    setInterval(countdown, 1000)
+    end = Date.now() + 5*1000*60; // 5 Minutes
+    setInterval(countdown, 1000);
 }
 function developer(){
-    var time = {k9000: 6,toaster: 6, hps400: 8, fp4125:7,delta3200:7,delta100:7,k100:7,k400:8,ktri400:8, a400:8, ilf400:7.5}
+    var time = {k9000: 6,toaster: .1, hps400: 8, fp4125:7,delta3200:7,delta100:7,k100:7,k400:8,ktri400:8, a400:8, ilf400:7.5}
     var recycle = false;
     hideElement('n0')
     end = Date.now() + (time[film]*1000*60);
-    setInterval(countdown, 1000)
+    do
+    { 
+        setInterval(countdown, 1000);
+    } while (end - Date.now() > 0);
 }
 
 function resolveTime(wait){
     min = Math.floor( wait / (1000*60))
     secs  = Math.floor( wait / 1000 );
     wait_sec = secs - min * 60;
-    if (wait_sec < 10){
-        wait_sec="0"+wait_sec}
+    if (wait_sec < 10)
+    {
+        wait_sec="0"+wait_sec
+    }
     return min + ":" +wait_sec
 }
 function countdown(min){
