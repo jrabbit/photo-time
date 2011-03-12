68fd04bac256289944d18c8e5322b2029e69d3c9
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-12 03:13:00 -0600

    ln options, speed up android deploy

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,14 +2,14 @@ task :default => [:android]
 
 desc "Build and install android version"
 task :android do
-  `cd Android && /usr/bin/ant debug install`
+  `cd Android && /usr/bin/ant install`
 end
 
 desc "Link JS/CSS/HTML into device resources"
 task :copy do
   puts "linking..."
   for file in ['favicon.ico',  'index.html',  'javascript.js',  'phototime.css'] do
-    cmd = "ln -s #{file} Android/assets/www/#{file}"
+    cmd = "ln #{file} Android/assets/www/#{file}"
     system cmd
   end
 end
