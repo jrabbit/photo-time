922c5c5a609be4751a9901732ea576df360121e4
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-12 01:24:55 -0600

    ¡RAKE VOODOO!

---
+++ Rakefile.rb
@@ -0,0 +1,14 @@
+task :default => [:android]
+
+desc "Build and install android version"
+task :android do
+  `cd Android && /usr/bin/ant debug install`
+end
+
+desc "Link JS/CSS/HTML into device resources"
+task :copy do
+  puts "linking..."
+  for file in ['favicon.ico',  'index.html',  'javascript.js',  'phototime.css'] do
+    cmd = "ln -s #{file} Android/assets/www/#{file}"
+  end
+end
