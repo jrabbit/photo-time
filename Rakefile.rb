8d83efdbd316e8bccae81e042bbdbaeddad24da5
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-17 01:22:54 -0500

    partial fix.

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,13 +5,19 @@ task :android do
   `cd Android && /usr/bin/ant install`
 end
 
-desc "Link JS/CSS/HTML into device resources"
+desc "copy JS/CSS/HTML into device resources"
 task :copy do
-  puts "linking..."
   Dir[Dir.pwd]
   # Files other than *.r* *.m*
   for file in Dir.glob("*.[^r|^m]*") do
-    cmd = "ln #{file} Android/assets/www/#{file}"
+    cmd = "cp #{file} Android/assets/www/#{file}"
     system cmd
+  end
+end
+
+task :rm do
+  Dir["Android/assets/www/"]
+  for file in Dir.glob("*[^phonegap].[^r|^m]*") do
+    system "rm #{file}"
   end
 end
