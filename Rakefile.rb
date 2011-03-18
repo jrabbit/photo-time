c7bb170ec82d7b5322fac63a2d0079d0db8ed924
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-18 16:12:54 -0500

    RAKE VOODOO DOCTOR HOUSE CALL

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,19 +5,25 @@ task :android => [:copy] do
   `cd Android && /usr/bin/ant install`
 end
 
+directory "Android/assets/www/js/"
+directory "Android/assets/www/css/"
+
 desc "copy JS/CSS/HTML into device resources"
-task :copy do
+
+
+task :copy => ["Android/assets/www/css/","Android/assets/www/js/"] do
   Dir[Dir.pwd]
   # Files other than *.r* *.m*
   for file in Dir.glob("*.[^r|^m]*") do
     cmd = "cp #{file} Android/assets/www/#{file}"
     system cmd
   end
+
   #copy folders.
-  for folder in ['js','css'] do
-    Dir[Dir.pwd + '/' + folder]
-    for file in Dir.glob("*.*") do
-      system "cp #{file} Android/assets/www/#{folder}/#{file}"
+  for folder in Dir['[^A|^t]*/'] do
+    for file in Dir[Dir.pwd + "/" + folder +"*"] do
+      name = file.split('/')[-1]
+      system "cp #{file} Android/assets/www/#{folder}#{name}"
     end
   end
 end
